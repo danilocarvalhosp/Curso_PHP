aa3483138dc4e1051fc4b63570b89e1cf86e9f3d
danilocarvalhosp <danilocarvalhosp@gmail.com>
2021-10-01 14:37:51 -0300

    Aula 04 - Tipos de variáveis

---
+++ Aula 04/index.php
@@ -0,0 +1,19 @@
+<?php 
+    // STRINGS
+    $nome = 'Guilherme';
+
+    // NÚMERO OU INTEIRO
+    $idade = 23;
+
+    // BOOLEANOS
+    $bool = true;
+
+    // CASAS DECIMAIS
+    $double = 10.09;
+
+    /*
+    echo 'Meu nome é ' .$nome;
+    echo 'Minha idade é '.$idade;
+    */
+    echo $bool;
+?>
