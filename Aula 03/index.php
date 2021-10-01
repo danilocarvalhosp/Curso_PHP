59241b4cf17f41a4c955fb3669269e110848d10a
danilocarvalhosp <danilocarvalhosp@gmail.com>
2021-10-01 14:13:17 -0300

    Aula 03 - Criando variáveis

---
+++ Aula 03/index.php
@@ -0,0 +1,22 @@
+<?php 
+    /*
+    $nome = 'Danilo';
+    echo 'Meu nome é <br />';
+    echo $nome;
+    
+    $nome = 'João';
+    echo '<br />';
+    echo 'Meu nome é <br />';
+    echo $nome;
+    */
+
+    // VALORES EM STRING
+    $nome = 'Guilherme';
+
+    // VALORES NUMÉRICOS
+    $idade = 23;
+
+    echo $nome;
+    echo '<br />';
+    echo $idade;
+?>
